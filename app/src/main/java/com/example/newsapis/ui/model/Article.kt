94ba005fc67f559b3c9434f551bb9e6606a6595d
Ahmed Khaled <ahmedhamtary@gmail.com>
2023-02-06 17:43:46 +0200

    Dao article

--- app/src/main/java/com/example/newsapis/ui/model/Article.kt
+++ app/src/main/java/com/example/newsapis/ui/model/Article.kt
@@ -1,6 +1,13 @@
 package com.example.newsapis.ui.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
+
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
