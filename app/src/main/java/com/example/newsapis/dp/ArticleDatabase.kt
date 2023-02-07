1764848567308d01890502e2e20ba3aa5c34e728
Ahmed Khaled <ahmedhamtary@gmail.com>
2023-02-07 09:37:19 +0200

    news adapter

---
+++ app/src/main/java/com/example/newsapis/dp/ArticleDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.newsapis.dp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.newsapis.ui.model.Article
+
+
+@Database(entities = [Article::class], version = 1)
+@TypeConverters(Converters::class)
+
+abstract class ArticleDatabase: RoomDatabase() {
+
+    abstract fun getArticlesDao(): ArticleDao
+
+    companion object{
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, ArticleDatabase::class.java, "article_dp.dp").build()
+    }
+}
